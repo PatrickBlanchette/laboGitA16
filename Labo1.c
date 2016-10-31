7c220b47c9feaa3a27e00273a2884e0f32cceb64
PatrickBlanchette <johndoe@example.com>
2016-10-31 18:17:26 -0400

    First commit

---
+++ Labo1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#define NOMBRE 10
+#define LONGUEUR_LIGNE 46
+
+int main(){
+  int i, j;
+  printf("  * |");
+  
+  for(i = 1; i < NOMBRE + 1; i++){
+    printf("%4d", i);
+  }
+  printf("\n");
+  
+  for(i = 1; i < LONGUEUR_LIGNE; i++){
+    printf("_");
+  }
+  printf("\n");
+  
+  for(i = 1; i < NOMBRE + 1; i++){
+    printf("%4d|", i);
+    
+    for(j = 1; j < NOMBRE + 1; j++){
+      printf("%4d", i * j);
+    }
+    printf("\n");
+  }
+  
+  
+	return 0;
+}
